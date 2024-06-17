c9f67133c51648ff6c5167b35b7845c970ad2df4
Kishan Raval <kishan.raval@technomark.io>
2024-06-17 18:26:01 +0530

    code update and changes
    code update and changes

---
+++ src/app/settings/organizations/edit/[id]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import ContentBlockForm from "../../OrganizationsBlockForm";
+
+const page = () => {
+  return (
+    <ContentBlockForm
+      open={1}
+      editingRow={null}
+      setEditingRow={() => {}}
+      handleClose={() => {}}
+    />
+  );
+};
+
+export default page;
