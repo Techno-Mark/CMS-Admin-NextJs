1437865cd5357135ca49b951b8c71b25f48dbced
vimalpatel123 <vimal.patel@technomark.io>
2024-07-15 18:05:38 +0530

    second level menu

--- src/app/content-management/menus/MenuItem.tsx
+++ src/app/content-management/menus/MenuItem.tsx
@@ -1,62 +1,89 @@
 "use client";
 import React, { useState } from "react";
 import DraggableIcon from "./_svg/_DraggableIcon";
+import { Fab } from "@mui/material";
+import KanbanDrawer from "./_kanban/KanbanDrawer";
 
 const MenuItem: React.FC = () => {
   const [menuItems, setMenuItems] = useState<any[]>([
     {
       name: "Item 1",
       logo: "",
-      link: "",
+      link: "/item-1",
       children: [
         {
           name: "Subitem 1-1",
           children: [],
           logo: "",
-          link: "",
+          link: "/item-1",
         },
-        { name: "Subitem 1-2", children: [], logo: "", link: "" },
-        { name: "Subitem 1-3", children: [], logo: "", link: "" },
+        { name: "Subitem 1-2", children: [], logo: "", link: "/item-1" },
+        { name: "Subitem 1-3", children: [], logo: "", link: "/item-1" },
       ],
     },
     {
       name: "Item 2",
-      children: [{ name: "Subitem 2-1", children: [], logo: "", link: "" }],
+      children: [
+        { name: "Subitem 2-1", children: [], logo: "", link: "/item-1" },
+      ],
       logo: "",
-      link: "",
+      link: "/item-1",
     },
     {
       name: "Item 3",
       children: [
-        { name: "Subitem 3-1", children: [], logo: "", link: "" },
-        { name: "Subitem 3-2", children: [], logo: "", link: "" },
+        { name: "Subitem 3-1", children: [], logo: "", link: "/item-1" },
+        { name: "Subitem 3-2", children: [], logo: "", link: "/item-1" },
       ],
       logo: "",
-      link: "",
+      link: "/item-1",
     },
   ]);
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [editDrawer, setEditDrawer] = useState(false);
+  const [editdata, setEditData] = useState<any>();
+
   const handleDragStart = (
     e: React.DragEvent<HTMLLIElement>,
-    item: any,
-    index: number
+    index: number,
+    parentId: number
   ) => {
-    e.dataTransfer.setData("text/plain", JSON.stringify(index));
+    const data = {
+      draggedIndex: index,
+      draggedItemParentIndex: parentId,
+    };
+    e.dataTransfer.setData("text/plain", JSON.stringify(data));
+  };
+
+  const handleDropOver = (
+    event: React.DragEvent<HTMLLIElement>,
+    targetIndex: number,
+    parentId: number
+  ) => {
+    event.preventDefault();
   };
 
   const handleDrop = (
     event: React.DragEvent<HTMLLIElement>,
-    targetItem: any,
-    targetIndex: number
+    targetIndex: number,
+    parentId: number
   ) => {
-    const draggedIndex: number = Number(
-      JSON.parse(event.dataTransfer.getData("text/plain"))
-    );
+    const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+
+    //Co-ordinates
+    const targetElement = event.currentTarget as HTMLElement;
+    const targetLeft = targetElement.getBoundingClientRect();
+    const currentLeft = event.pageX;
+
+    if (targetLeft.left + 180 > currentLeft) {
+      console.log(targetLeft.left, currentLeft);
+    }
 
     const newMenuItems = [...menuItems];
 
-    const removedItem = removeMenuItem(newMenuItems, draggedIndex);
-    addMenuItem(newMenuItems, removedItem, targetIndex);
+    const removedItem = removeMenuItem(newMenuItems, data);
+    addMenuItem(newMenuItems, removedItem, targetIndex, parentId);
 
     return;
   };
@@ -64,28 +91,52 @@ const MenuItem: React.FC = () => {
   const addMenuItem = (
     newMenuItems: any,
     removedItem: any,
-    targetIndex: number
+    targetIndex: number,
+    parentId: number
   ) => {
-    newMenuItems.splice(targetIndex, 0, removedItem);
-    console.log(newMenuItems);
-    setMenuItems(newMenuItems);
-  };
-  const removeMenuItem = (newMenuItems: any, draggedIndex: number) => {
-    const removedItem = menuItems[draggedIndex];
-    newMenuItems.splice(draggedIndex, 1);
-    return removedItem;
+    if (parentId == -1) {
+      newMenuItems.splice(targetIndex, 0, removedItem);
+      setMenuItems(newMenuItems);
+    } else {
+      newMenuItems[parentId].children.splice(targetIndex, 0, removedItem);
+      setMenuItems(newMenuItems);
+      console.log(targetIndex, parentId);
+    }
   };
 
-  const renderMenuItems = (items: any[]): React.ReactNode => {
+  const removeMenuItem = (newMenuItems: any, data: any) => {
+    if (data.draggedItemParentIndex == -1) {
+      const removedItem = menuItems[data.draggedIndex];
+      newMenuItems.splice(data.draggedIndex, 1);
+      return removedItem;
+    } else {
+      const removedItem =
+        newMenuItems[data.draggedItemParentIndex].children[data.draggedIndex];
+      newMenuItems[data.draggedItemParentIndex].children.splice(
+        data.draggedIndex,
+        1
+      );
+      return removedItem;
+    }
+  };
+
+  const handleEdit = (index: number, parentId: number) => {
+    setEditDrawer(true);
+    setEditData({ index: index, parentId: parentId });
+  };
+
+  const handleDelete = () => {};
+
+  const renderMenuItems = (items: any[], parentId: number): React.ReactNode => {
     return items.map((item, index) => (
       <ul key={index}>
         {" "}
         <li
           className="flex items-center border-b-black border-b p-1"
           draggable
-          onDragStart={(e) => handleDragStart(e, item, index)}
-          onDrop={(e) => handleDrop(e, item, index)}
-          onDragOver={(e: React.DragEvent<HTMLLIElement>) => e.preventDefault()}
+          onDragStart={(e) => handleDragStart(e, index, parentId)}
+          onDrop={(e) => handleDrop(e, index, parentId)}
+          onDragOver={(e) => handleDropOver(e, index, parentId)}
         >
           <div className="flex-1 flex items-center">
             <DraggableIcon />
@@ -93,7 +144,10 @@ const MenuItem: React.FC = () => {
           </div>
           <div className="flex rounded-md border cursor-pointer">
             <div className=" bg-white border-r p-1">
-              <button className="bg-white text-black cursor-pointer">
+              <button
+                className="bg-white text-black cursor-pointer"
+                onClick={() => handleEdit(index, parentId)}
+              >
                 Edit
               </button>
             </div>
@@ -102,13 +156,41 @@ const MenuItem: React.FC = () => {
             </div>
           </div>
         </li>
-        {item.children && renderMenuItems(item.children)}
+        {item.children && renderMenuItems(item.children, index)}
       </ul>
     ));
   };
   return (
-    <div className="bg-white p-3 max-w-[70%] m-auto">
-      {renderMenuItems(menuItems)}
+    <div className="bg-white p-2 max-w-[70%] m-auto">
+      {renderMenuItems(menuItems, -1)}
+      <Fab
+        variant="extended"
+        className="w-7 h-7"
+        onClick={() => setDrawerOpen(true)}
+      >
+        <i className="tabler-plus mie-1" />
+        Add
+      </Fab>
+      {drawerOpen && (
+        <KanbanDrawer
+          drawerOpen={drawerOpen}
+          setDrawerOpen={setDrawerOpen}
+          dataRequired={null}
+          menuItems={menuItems}
+          setMenuItems={setMenuItems}
+          open={-1}
+        />
+      )}
+      {editDrawer && (
+        <KanbanDrawer
+          drawerOpen={editDrawer}
+          setDrawerOpen={setEditDrawer}
+          dataRequired={editdata}
+          menuItems={menuItems}
+          setMenuItems={setMenuItems}
+          open={1}
+        />
+      )}
     </div>
   );
 };
