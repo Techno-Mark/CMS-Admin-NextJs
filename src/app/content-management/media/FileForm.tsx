7775a489f3be102866ff73f9e6765d0e9f64a651
Kishan Raval <kishan.raval@technomark.io>
2024-09-16 18:12:14 +0530

    remove preview icon in media

--- src/app/content-management/media/FileForm.tsx
+++ src/app/content-management/media/FileForm.tsx
@@ -178,11 +178,11 @@ function FileForm({ open, editingRow, handleClose ,permissionUser}: blogFormProp
                     <Grid item xs={12} sm={11}>
                         <BreadCrumbList />
                     </Grid>
-                    <Grid item xs={12} sm={1}>
+                    {/* <Grid item xs={12} sm={1}>
                         <IconButton color="info" onClick={() => { }}>
                             <i className="tabler-external-link text-textSecondary"></i>
                         </IconButton>
-                    </Grid>
+                    </Grid> */}
                 </Grid>
             </Box>
             <Card className="p-4">
