352a6a275da9a2860b0865a7ddbf954f0b096888
vimalpatel-dev <vimal.patel@technomark.io>
2024-09-25 11:43:40 +0530

    bug solving user, menu

--- src/app/content-management/menus/MenuForm.tsx
+++ src/app/content-management/menus/MenuForm.tsx
@@ -1,6 +1,6 @@
 // components/OrganizationsForm.tsx
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Typography,
@@ -8,92 +8,111 @@ import {
   Switch,
   Button,
   Card,
-  MenuItem
-} from "@mui/material"
-import { toast } from "react-toastify"
-import { post } from "@/services/apiService"
-import CustomTextField from "@/@core/components/mui/TextField"
-import BreadCrumbList from "@/components/BreadCrumbList"
-import LoadingBackdrop from "@/components/LoadingBackdrop"
-import { menuDetailType } from "@/types/apps/menusType"
-import { menu } from "@/services/endpoint/menu"
+  MenuItem,
+} from "@mui/material";
+import { toast } from "react-toastify";
+import { post } from "@/services/apiService";
+import CustomTextField from "@/@core/components/mui/TextField";
+import BreadCrumbList from "@/components/BreadCrumbList";
+import LoadingBackdrop from "@/components/LoadingBackdrop";
+import { menuDetailType } from "@/types/apps/menusType";
+import { menu } from "@/services/endpoint/menu";
+import ActiveConfirmationDialog from "./ActiveConfirmationDialog";
 
 type Props = {
   open: -1 | 0 | 1;
   handleClose: () => void;
   editingRow: menuDetailType | null;
-  permissionUser: Boolean
+  permissionUser: Boolean;
 };
 
 const initialData = {
   menuId: -1,
   menuName: "",
   menuLocation: "",
-  active: false
-}
+  active: false,
+};
 
 const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
-  const [loading, setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isActiveConfirm, setIsActiveConfirm] = useState(false);
 
-  const [formData, setFormData] = useState<typeof initialData>(initialData)
+  const [formData, setFormData] = useState<typeof initialData>(initialData);
   const [formErrors, setFormErrors] = useState({
     menuName: "",
     menuLocation: "",
-    active: ""
-  })
+    active: "",
+  });
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     if (editingRow) {
-      setFormData(editingRow)
-      setLoading(false)
+      setFormData(editingRow);
+      setLoading(false);
     } else {
-      setFormData(initialData)
-      setLoading(false)
+      setFormData(initialData);
+      setLoading(false);
     }
-  }, [editingRow])
+  }, [editingRow]);
 
   const validateFormData = (data: menuDetailType) => {
-    let isValid = true
-    const errors = { ...formErrors }
+    let isValid = true;
+    const errors = { ...formErrors };
 
     if (data.menuName.trim().length < 5) {
-      errors.menuName = "Menu Name must be at least 5 characters long"
-      isValid = false
+      errors.menuName = "Menu Name must be at least 5 characters long";
+      isValid = false;
     }
     if (!data.menuLocation) {
-      errors.menuLocation = "Menu Location is required"
-      isValid = false
+      errors.menuLocation = "Menu Location is required";
+      isValid = false;
     }
-    setFormErrors(errors)
-    return isValid
+    setFormErrors(errors);
+    return isValid;
+  };
+
+
+  const handleSubmitAPICall = async () => {
+    try {
+      setLoading(true);
+      const endpoint = menu.createAndUpdate;
+
+      const payload = {
+        menuId: editingRow ? formData.menuId : undefined,
+        menuName: formData.menuName,
+        menuLocation: formData.menuLocation,
+        active: formData.active,
+      };
+
+      const response = await post(endpoint, payload);
+
+      toast.success(response.message);
+      handleClose();
+      setFormData(response);
+      setLoading(false);
+    } catch (error: any) {
+      console.error("Error fetching data:", error.message);
+      setLoading(false);
+    }
   }
+
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
     if (validateFormData(formData)) {
       try {
-        setLoading(true)
-        const endpoint = menu.createAndUpdate
 
-        const payload = {
-          menuId: editingRow ? formData.menuId : undefined,
-          menuName: formData.menuName,
-          menuLocation: formData.menuLocation,
-          active: formData.active
+        if(editingRow && formData.active === true){
+          setIsActiveConfirm(true);
+        }else{
+          handleSubmitAPICall();
         }
 
-        const response = await post(endpoint, payload)
-
-        toast.success(response.message)
-        handleClose()
-        setFormData(response)
-        setLoading(false)
       } catch (error: any) {
-        console.error("Error fetching data:", error.message)
-        setLoading(false)
+        console.error("Error fetching data:", error.message);
+        setLoading(false);
       }
     }
-  }
+  };
 
   return (
     <>
@@ -115,12 +134,12 @@ const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
                     onChange={(e) => {
                       setFormErrors((prevErrors) => ({
                         ...prevErrors,
-                        menuName: ""
-                      }))
+                        menuName: "",
+                      }));
                       setFormData((prevData) => ({
                         ...prevData,
-                        menuName: e.target.value
-                      }))
+                        menuName: e.target.value,
+                      }));
                     }}
                   />
                 </Grid>
@@ -137,12 +156,12 @@ const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
                     onChange={(e) => {
                       setFormErrors((prevErrors) => ({
                         ...prevErrors,
-                        menuLocation: ""
-                      }))
+                        menuLocation: "",
+                      }));
                       setFormData((prevData) => ({
                         ...prevData,
-                        menuLocation: e.target.value
-                      }))
+                        menuLocation: e.target.value,
+                      }));
                     }}
                   >
                     <MenuItem value={"header"}>header</MenuItem>
@@ -150,18 +169,20 @@ const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
                   </CustomTextField>
                 </Grid>
 
-                <Grid item xs={12} sm={2}>
-                  <Typography variant="body2" sx={{ mr: 0 }}>
-                    Status
-                  </Typography>
-                  <Switch
-                    size="medium"
-                    checked={formData.active}
-                    onChange={(e) =>
-                      setFormData({ ...formData, active: e.target.checked })
-                    }
-                  />
-                </Grid>
+                {editingRow && (
+                  <Grid item xs={12} sm={2}>
+                    <Typography variant="body2" sx={{ mr: 0 }}>
+                      Status
+                    </Typography>
+                    <Switch
+                      size="medium"
+                      checked={formData.active}
+                      onChange={(e) =>
+                        setFormData({ ...formData, active: e.target.checked })
+                      }
+                    />
+                  </Grid>
+                )}
               </Grid>
             </Box>
 
@@ -180,7 +201,7 @@ const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
                 <Button variant="outlined" color="error" onClick={handleClose}>
                   Cancel
                 </Button>
-                {permissionUser &&
+                {permissionUser && (
                   <Button
                     variant="contained"
                     type="submit"
@@ -188,14 +209,20 @@ const MenuForm = ({ open, handleClose, editingRow, permissionUser }: Props) => {
                   >
                     {open === -1 ? "Add" : "Edit"} Menu
                   </Button>
-                }
+                )}
               </Box>
             </Grid>
           </div>
         </form>
       </Card>
+      {
+        isActiveConfirm &&
+        <ActiveConfirmationDialog setOpen={setIsActiveConfirm}
+        menuLocation={formData.menuLocation}
+        handleSubmit={handleSubmitAPICall} />
+      }
     </>
-  )
-}
+  );
+};
 
-export default MenuForm
+export default MenuForm;
