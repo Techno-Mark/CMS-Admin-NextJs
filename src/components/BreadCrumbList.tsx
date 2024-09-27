29ef70540454467a5903198e73ef977db535e70b
vimalpatel-dev <vimal.patel@technomark.io>
2024-09-27 10:22:26 +0530

    lint fix

--- src/components/BreadCrumbList.tsx
+++ src/components/BreadCrumbList.tsx
@@ -1,18 +1,18 @@
-import { Typography } from "@mui/material";
-import { usePathname, useRouter } from "next/navigation";
+import { Typography } from "@mui/material"
+import { usePathname, useRouter } from "next/navigation"
 
 const BreadCrumbList = () => {
-  const router = useRouter();
-  const pathSplits: string[] = usePathname().substring(1).split("/");
+  const router = useRouter()
+  const pathSplits: string[] = usePathname().substring(1).split("/")
 
   const breadCrumbClick = (clickedItemIndex: number): void => {
-    const pathSegments = pathSplits.slice(0, clickedItemIndex + 1).join("/");
-    router.push(`/${pathSegments}`);
-  };
+    const pathSegments = pathSplits.slice(0, clickedItemIndex + 1).join("/")
+    router.push(`/${pathSegments}`)
+  }
 
   const formatBreadcrumb = (text: string) => {
-    return text.replace(/-/g, " ");
-  };
+    return text.replace(/-/g, " ")
+  }
 
   return (
     <div className="h-14 flex items-center">
@@ -20,7 +20,7 @@ const BreadCrumbList = () => {
         <div
           key={index}
           onClick={() => {
-            let editIndex = pathSplits.indexOf("edit");
+            const editIndex = pathSplits.indexOf("edit")
             if (
               index === 0 ||
               (item.toLowerCase() === "edit" &&
@@ -28,9 +28,9 @@ const BreadCrumbList = () => {
               (editIndex !== -1 && pathSplits.indexOf("edit") <= index) ||
               !Number.isNaN(parseInt(pathSplits[index]))
             ) {
-              return;
+              return
             }
-            breadCrumbClick(index);
+            breadCrumbClick(index)
           }}
         >
           <Typography
@@ -43,9 +43,7 @@ const BreadCrumbList = () => {
                 (item.toLowerCase() === "edit" &&
                   index === pathSplits.length - 2) ||
                 (pathSplits.indexOf("edit") !== -1 &&
-                  pathSplits.indexOf("edit") <= index)
-                  ? "none"
-                  : "auto",
+                  pathSplits.indexOf("edit") <= index) ? "none" : "auto"
             }}
           >
             &nbsp;{formatBreadcrumb(item)}&nbsp;
@@ -54,7 +52,7 @@ const BreadCrumbList = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default BreadCrumbList;
+export default BreadCrumbList
